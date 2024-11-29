1078d55f373f30f3d004fdf9265aaa435ac320e0
chenmaol <chenmaol@nvidia.com>
2024-11-29 18:55:56 +0800

    v2.0.0-rc7

--- client/tests/test_capture_image.py
+++ client/tests/test_capture_image.py
@@ -2,11 +2,15 @@ import cv2
 
 import os
 import sys
+import yaml
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from env import WRCGContinuousEnv
 
+with open("../config.yaml", 'r') as f:
+    config = yaml.load(f.read(), Loader=yaml.FullLoader)
 
-env = WRCGContinuousEnv()
+env = WRCGContinuousEnv(config["env"])
 image = env.get_frame()
 print(image.shape)
 cv2.imwrite("test_capture_image.jpg", image)
